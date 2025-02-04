59006ecf5a1de8ee9e72a89a4a0aeb556ee339ec
Noam Boulze <noamboulze@gmail.com>
2025-02-04 14:13:48 +0100

    initial commit

---
+++ src/config.mjs
@@ -0,0 +1,12 @@
+export default {
+  development: {
+    type: 'development',
+    port: 3000,
+    mongodb: 'mongodb+srv://noamboulze:gupiE7WmCamkH1Nu@efrei.q2kyc.mongodb.net/api'
+  },
+  production: {
+    type: 'production',
+    port: 3000,
+    mongodb: 'mongodb+srv://noamboulze:gupiE7WmCamkH1Nu@efrei.q2kyc.mongodb.net/api'
+  }
+};
